5000d712f2fd4d18960ed5246ebbd67a67149008
fauky <fmented@gmail.com>
2021-08-06 17:36:29 +0700

    xxxxxx

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ const fs = require("fs");
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree=", `'${folderName}'`, "checkout", "master"]);
+    await execa("git", ["--work-tree='build'", "checkout", "master"]);
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
